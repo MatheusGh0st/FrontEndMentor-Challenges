b9b36353af0c60de6ed926af737fb12dd6900132
MatheusGh0st <matheusghost88@gmail.com>
2023-10-05 11:37:11 -0300

    fix bugs in css

--- age-calculator-app/src/components/Calculator.js
+++ age-calculator-app/src/components/Calculator.js
@@ -4,10 +4,11 @@ import './Calculator.css';
 
 export default function Calculator() {
     const [formDate, setFormDate] = React.useState({
-        day: '',
-        month: '',
-        year: ''
+        day: "",
+        month: "",
+        year: "",
     });
+
 
     const [dates, setDates] = React.useState({
         day: '--',
@@ -15,24 +16,27 @@ export default function Calculator() {
         year: '--'
     });
 
+
     const [errorMsg, setErrorMsg] = React.useState({
         day: "",
         month: "",
         year: "",
     });
 
+
     function isInRange(num, min, max) {
         return num >= Math.min(min, max) && num <= Math.max(min, max);
     }
 
-    function defineMessagesInvalid(date) {
+
+    function defineMessagesInvalid() {
         const objMsg = { 
             day: "",
             month: "",
             year: ""
         }
 
-        const { day, month, year } = date;
+        const { day, month, year } = formDate;
         const dayMax = new Date(year, month, 0).getDate();
         const yearMax = new Date().getFullYear();
 
@@ -49,6 +53,7 @@ export default function Calculator() {
         return objMsg;
     }
 
+
     function handleChangeForm(event) {
         const { value, name } = event.target;
         setFormDate(prevFormDate => {
@@ -58,6 +63,7 @@ export default function Calculator() {
             };
         });
     }
+
 
     function defineMessagesEmpty() {
         const objMsg = { 
@@ -75,88 +81,150 @@ export default function Calculator() {
         return objMsg;
     }
 
+
     function isDateEmpty() {
        if (!formDate.day || !formDate.month || !formDate.year) return true;
        return false;
     }
 
-    function getResult() {
-        
-        if (!isDateEmpty()) {
-            const d = formDate;
-            const dateForm = new Date(`${d.year}/${d.month}/${d.day}`);
-            const currentDate = new Date();
 
-            const timeStampF = dateForm.getTime();
-            const timeStampC = currentDate.getTime();
+    function calcAndSetDates() {
+        const d = formDate;
+        const dateForm = new Date(`${d.year}/${d.month}/${d.day}`);
+        const currentDate = new Date();
 
-            const difference = timeStampC - timeStampF;
+        const timeStampF = dateForm.getTime();
+        const timeStampC = currentDate.getTime();
 
-            if (difference < 0) difference = -difference;
+        const difference = timeStampC - timeStampF;
 
-            const days = Math.floor(difference / (1000 * 60 * 60 * 24));
-            const months = Math.floor(days / 31);
-            const years = Math.floor(months / 12);
+        if (difference < 0) difference = -difference;
 
-            setDates(_ => {
-                return {
-                    day: days % 31,
-                    month: months % 12,
-                    year: years
-                }
-            });
-        }
-        
-        const objError = defineMessagesEmpty();
-        
-        setErrorMsg(_ => {
+        const days = Math.floor(difference / (1000 * 60 * 60 * 24));
+        const months = Math.floor(days / 31);
+        const years = Math.floor(months / 12);
+
+        setDates(_ => {
             return {
-                day: objError.day,
-                month: objError.month,
-                year: objError.year
+                day: days % 31,
+                month: months % 12,
+                year: years
             }
         });
     }
+
+
+    function isDateInvalid() {
+        const { day, month, year } = formDate;
+        const dayMax = new Date(year, month, 0).getDate();
+        const yearMax = new Date().getFullYear();
+
+        if (!isInRange(day, 1, dayMax) || 
+            !isInRange(month, 1, 12) || 
+            !isInRange(year, 1, yearMax)) return true;
+
+        return false; 
+    }
+
+
+    function setInvalidMsg() {
+        const errorInvalid = defineMessagesInvalid();
+
+        setErrorMsg(_ => {
+            return {
+                ...errorInvalid
+            }
+        });
+    }
+
+
+    function resetStateMsg() {
+        setEmptyMsg(_ => {
+            return {
+                day: "",
+                month: "",
+                year: ""
+            }
+        });
+    }
+
+
+    function setEmptyMsg() {
+        const errorEmpty = defineMessagesEmpty();
+        
+        setErrorMsg(_ => {
+            return {
+                day: errorEmpty.day,
+                month: errorEmpty.month,
+                year: errorEmpty.year
+            }
+        });
+    }
+
+
+    function getResult() {
+
+        resetStateMsg();
+        
+        if (!isDateEmpty()) {
+
+            if (!isDateInvalid()) {
+                calcAndSetDates();
+            } else {
+                setInvalidMsg();
+            }
+        } else {
+            setEmptyMsg();
+        }
+
+    }
+
 
     return (
         <>
             <div className="calculator-container">
                 <div className="calculator-card">
                     <div className="date-card">
-                        <label className="date-label">Day</label>
-                        <input
-                            className="date-input"
-                            type="number"
-                            placeholder="DD"
-                            onChange={handleChangeForm}
-                            name="day"
-                            value={formDate.day}
-                        />
-                        <span className="error-msg">{errorMsg.day}</span>
+                        <label className={`date-label ${errorMsg.day.length ? "date-label-error" : ""}`}>Day</label>
+                        <div className="input-container">
+                            <input
+                                className={`date-input ${errorMsg.day.length ? "date-input-error" : ""}`}
+                                type="number"
+                                placeholder="DD"
+                                onChange={handleChangeForm}
+                                name="day"
+                                value={formDate.day}
+                            />
+                            <span className="error-msg">{errorMsg.day}</span>
+                        </div>
                     </div>
                     <div className="date-card">
-                        <label className="date-label">Month</label>
-                        <input
-                            className="date-input"
-                            type="number"
-                            placeholder="MM"
-                            onChange={handleChangeForm}
-                            name="month"
-                            value={formDate.month}
-                        />
-                        <span className="error-msg">{errorMsg.month}</span>
+                        <label className={`date-label ${errorMsg.month.length ? "date-label-error" : ""}`}>Month</label>
+                        <div className="input-container">
+                            <input
+                                className={`date-input ${errorMsg.month.length ? "date-input-error" : ""}`}
+                                type="number"
+                                placeholder="MM"
+                                onChange={handleChangeForm}
+                                name="month"
+                                value={formDate.month}
+                            />
+                            <span className="error-msg">{errorMsg.month}</span>
+                        </div>
                     </div>
                     <div className="date-card">
-                        <label className="date-label">Year</label>
-                        <input
-                            className="date-input"
-                            type="number"
-                            placeholder="YYYY"
-                            onChange={handleChangeForm}
-                            name="year"
-                            value={formDate.year}
-                        />
-                        <span className="error-msg">{errorMsg.year}</span>
+                        <label className={`date-label ${errorMsg.year.length ? "date-label-error" : ""}`}>Year</label>
+                        <div className="input-container">
+                            <input 
+                                className={`date-input ${errorMsg.year.length ? "date-input-error" : ""}`}
+                                type="number"
+                                placeholder="YYYY"
+                                onChange={handleChangeForm}
+                                name="year"
+                                value={formDate.year}
+                            />
+                            <span className="error-msg">{errorMsg.year}</span>
+                        </div>
                     </div>
                 </div>
 
