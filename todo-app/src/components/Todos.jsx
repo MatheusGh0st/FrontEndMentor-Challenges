eedbb920a4c7f8c955a4258cdd66d80726450502
MatheusGh0st <matheusghost88@gmail.com>
2023-12-31 11:04:12 -0300

    add todo btns states

--- todo-app/src/components/Todos.jsx
+++ todo-app/src/components/Todos.jsx
@@ -4,10 +4,19 @@ import Checkbox from "./Checkbox";
 export default function FormCheckBox() {
     const [todos, setTodos] = useState([]);
     const [formTitle, setFormTitle] = useState("");
+    const [btnState, setBtnState] = useState({ all: false, active: false, completed: false });
 
     const handleInputChange = (e) => {
         setFormTitle(e.target.value);
     };
+
+    const handleBtnChange = (e) => {
+        const { name } = e.target;
+
+        setBtnState(prevBtnState => {
+            return { ...prevBtnState, [name]: true };
+        })
+    }
 
     const handleKeyUp = (e) => {
         if (e.key === 'Enter' && formTitle.trim() !== "") {
@@ -17,16 +26,36 @@ export default function FormCheckBox() {
     };
 
     return (
-        <div className="todo-checkbox-container">
-            <input
-                value={formTitle}
-                onChange={handleInputChange}
-                onKeyUp={handleKeyUp}
-                placeholder="Create a new todo..."
-            />
-            {todos.map((todo, index) => (
-                <div className="todo-checkbox" key={index}>{todo}</div>
-            ))}
-        </div>
+        <>
+            <div className="todo-checkbox-container">
+                <input
+                    value={formTitle}
+                    onChange={handleInputChange}
+                    onKeyUp={handleKeyUp}
+                    placeholder="Create a new todo..."
+                />
+
+                { todos.map((todo, index) => (
+                    <div className="todo-checkbox" key={index}>{todo}</div>
+                ))}
+            </div>
+            <div className="todos-btn">
+                <button
+                    name="all"
+                    type="button"
+                    onClick={handleBtnChange}
+                    >All</button>
+                <button
+                    name="active"
+                    type="button"
+                    onClick={handleBtnChange}
+                    >Active</button>
+                <button
+                    name="completed"
+                    type="button"
+                    onClick={handleBtnChange}
+                    >Completed</button>
+            </div>
+        </>
     );
 }
